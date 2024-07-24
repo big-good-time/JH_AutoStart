c015c5eb67faf47e3e9de8d323bf5b3f42141709
big-good-time <17600037754@163.com>
2024-07-24 12:28:26 +0800

    如果没有配置文件就创建一个默认配置文件

--- DataMode.py
+++ DataMode.py
@@ -5,13 +5,22 @@ class DataMode():
         self.loadData()
 
     def loadData(self) -> dict:
-        with open('./data.json', 'r') as f:
-            self.data = json.loads(f.read())
-        return self.data
+        try:
+            with open('./data.json', 'r') as f:
+                self.data = json.loads(f.read())
+            return self.data
+        except:
+            self.initData()
 
     def updateData(self):
         with open('./data.json', 'w') as f:
             json.dump(self.data, f)
+    
+    def initData(self):
+        """重置配置文件"""
+        self.data = {"waitTime": 120, "spaceTime": 5, "pathList": [], "pathEditWidth": 400, "noteEditWidth": 100, "windowWidth": 800, "windowHeight": 400}
+        self.updateData()
+
 
 if __name__ == '__main__':
     dm = DataMode()
